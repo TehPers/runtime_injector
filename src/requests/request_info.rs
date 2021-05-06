d137701e166fd517902c0be75cd9a35f7b3c5fd6
TehPers <tehperz@gmail.com>
2021-05-06 01:40:24 -0700

    Added request parameters

--- src/requests/request_info.rs
+++ src/requests/request_info.rs
@@ -1,9 +1,44 @@
 use crate::ServiceInfo;
+use std::{any::Any, collections::HashMap, fmt::Debug};
+
+/// A parameter for configuring requested services.
+pub trait RequestParameter: Any + Debug {
+    /// Clones this parameter into a boxed trait object.
+    fn clone_dyn(&self) -> Box<dyn RequestParameter>;
+
+    /// Casts this parameter into a [`&dyn Any`](Any), making it easier to
+    /// downcast into other types.
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl<T: Any + Debug + Clone> RequestParameter for T {
+    fn clone_dyn(&self) -> Box<dyn RequestParameter> {
+        Box::new(self.clone())
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl dyn RequestParameter {
+    /// Tries to downcast this request parameter to a concrete type.
+    pub fn downcast_ref<T: RequestParameter>(&self) -> Option<&T> {
+        self.as_any().downcast_ref()
+    }
+}
+
+impl Clone for Box<dyn RequestParameter> {
+    fn clone(&self) -> Self {
+        self.clone_dyn()
+    }
+}
 
 /// Information about an active request.
 #[derive(Clone, Debug)]
 pub struct RequestInfo {
     service_path: Vec<ServiceInfo>,
+    parameters: HashMap<String, Box<dyn RequestParameter>>,
 }
 
 impl RequestInfo {
@@ -12,7 +47,18 @@ impl RequestInfo {
     pub fn new() -> Self {
         RequestInfo {
             service_path: Vec::new(),
+            parameters: HashMap::new(),
         }
+    }
+
+    /// Sets a request parameter for the request. If a parameter has already
+    /// been set to a value, then that value is returned.
+    pub fn insert_parameter(
+        &mut self,
+        key: &str,
+        value: impl RequestParameter,
+    ) -> Option<Box<dyn RequestParameter>> {
+        self.parameters.insert(key.to_owned(), Box::new(value))
     }
 
     /// Creates a new child instance of [`RequestInfo`] with the given service
@@ -66,6 +112,13 @@ impl RequestInfo {
     #[must_use]
     pub fn service_path(&self) -> &[ServiceInfo] {
         &self.service_path
+    }
+
+    /// Gets the value of a parameter if it has been set.
+    pub fn get_parameter(&self, key: &str) -> Option<&dyn RequestParameter> {
+        self.parameters
+            .get(key)
+            .map(|parameter| parameter.as_ref())
     }
 }
 
